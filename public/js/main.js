c4fd60b1cd903f8017082967673bad6e52bc00cf
orlando leyva <leyvaorlando10@gmail.com>
2022-06-12 09:21:08 -0700

    efecto smooth y header fixed

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+var _a;
+(_a = document.getElementById('statistics-link')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    window.scroll({
+        behavior: 'smooth'
+    });
+});
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+document.getElementById('statistics-link')?.addEventListener('click', ()=>{
+    window.scroll({
+        behavior: 'smooth'
+    })
+    
+})
